7ed1ec89d186b351df84d44db8f39fc6364748ac
Carol.Li <abc61958761@gmail.com>
2021-04-26 10:28:38 +0800

    Modify Page

--- src/api/appserve.js
+++ src/api/appserve.js
@@ -3,5 +3,5 @@ import axios from "axios";
 const appserve = axios.create({
   baseURL: process.env.VUE_APP_BASEURL,
 });
-
+console.log(process.env.VUE_APP_BASEURL)
 export default appserve;
--- src/router.js
+++ src/router.js
@@ -1,18 +1,18 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import HelloWorld from "./views/HelloWorld";
+
 import PurchaseRecord from "./views/PurchaseRecord";
 import Inventory from "./views/Inventory";
 import SoldRecord from "./views/SoldRecord";
 import SettlementRecrod from "./views/SettlementRecrod";
+
 Vue.use(VueRouter);
 
 const routes = [
-  { path: "/", component: HelloWorld },
-  { path: "/purchase-record", component: PurchaseRecord },
+  { path: "/", component: PurchaseRecord, alias: "/purchase-record" },
   { path: "/inventory", component: Inventory },
   { path: "/sold-record", component: SoldRecord },
-  { path: "/settlement-recrod", component: SettlementRecrod }
+  { path: "/settlement-recrod", component: SettlementRecrod },
 ];
 
 const router = new VueRouter({
